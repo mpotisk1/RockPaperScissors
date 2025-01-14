0776196a19b5310f74fa91ea958bb48787326b62
mpotisk1 <mpotisk2@gmail.com>
2025-01-13 19:44:28 -0600

    made a rock paper scissors console game

---
+++ rps.js
@@ -0,0 +1,105 @@
+console.log("Hello World!");
+
+let humanScore = 0;
+let computerScore = 0;
+
+let computerChoice = "";
+let humanChoice = "";
+
+
+function getComputerChoice(){
+    let computerNumChoice = 0;
+    computerNumChoice = Math.floor(Math.random() * 10);
+    if (computerNumChoice === 0){
+        return getComputerChoice()
+    }
+    else if (computerNumChoice === 1 ||
+        computerNumChoice === 2 ||
+        computerNumChoice === 3
+    ){
+        computerChoice = "rock";
+    }
+    else if (computerNumChoice === 4 || 
+        computerNumChoice === 5 ||
+        computerNumChoice === 6){
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors"
+    };
+
+   // console.log(computerChoice);
+    return computerChoice;
+};
+
+function getHumanChoice(){
+    let humanChoice = prompt("type 'rock' 'paper' or 'scissors'");
+   // console.log(humanChoice);
+    return(humanChoice);
+
+}
+
+
+
+function playRound(){
+     humanChoice = getHumanChoice();
+     computerChoice = getComputerChoice();
+
+if (computerChoice == "rock"){
+    if (humanChoice == "rock"){
+        console.log("Tie!");
+    }
+    else if (humanChoice == "paper"){
+        console.log("You Win!");
+        humanScore++;
+    }
+    else {console.log("You Lose!");
+        computerScore++;
+}
+}
+else if (computerChoice == "paper"){
+    if (humanChoice == "rock"){
+        console.log("You Lose!");
+        computerScore++;
+    }
+    else if (humanChoice == "paper"){
+        console.log("Tie!")}
+    else {
+        console.log("You Win!");
+        humanScore++;
+}
+}
+else if (computerChoice == "scissors"){
+    if (humanChoice == "rock"){
+        console.log("You Win!");
+        humanScore++;
+    }
+    else if (humanChoice == "paper"){
+        console.log("You Lose!");
+        computerScore++;
+}
+    else {console.log("Tie!")
+    }
+}
+}
+function playGame(){
+
+
+        for (i = 0; i <= 5; i++){
+        playRound();
+        console.log(humanChoice + " Vs " + computerChoice + "...." + " The Score is now " + humanScore + " to " + computerScore);
+    }
+        if (humanScore > computerScore) {
+            console.log("Good Job! You have Defeated AI!")
+        }
+        else if (humanScore < computerScore) {
+            console.log("You've been deafeated. Prepare for the AI takeover.")
+        }
+        else {
+            console.log("A tie after 5 rounds? One more. This is your chance to Redeem yourself.");
+            playRound();
+        }
+    
+    
+}
+playGame();
