abad8562a4ceeebf2299902c2ad292b35c003659
mpotisk1 <mpotisk2@gmail.com>
2025-02-03 16:31:45 -0600

    began adding UI, changed fucntions to update innerText of <p> elements

--- rps.js
+++ rps.js
@@ -6,11 +6,20 @@ let computerScore = 0;
 let computerChoice = "";
 let humanChoice = "";
 let roundNum = 0;
-
+//buttons
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scisButton = document.getElementById('scissors');
 
+
+//UI
+const humanUi = document.getElementById('humanScore');
+const computerUi = document.getElementById('computerScore');
+const roundUi = document.getElementById('round');
+const winner = document.getElementById('winner');
+
+
+//Button events
 rockButton.onclick = () => {humanChoice = "rock", playRound()};
 paperButton.onclick = () => {humanChoice = "paper", playRound()};
 scisButton.onclick = () => {humanChoice = "scissors", playRound()};
@@ -42,6 +51,22 @@ function getComputerChoice(){
     return computerChoice;
 };
 
+function scoreUpdt(){
+    humanUi.innerText = humanScore;
+    computerUi.innerText = computerScore;
+}
+
+function win(){
+    console.log("You Win!");
+    humanScore++;
+    scoreUpdt();
+}
+
+function lose(){
+    console.log("You Lose!");
+    computerScore++;
+    scoreUpdt();
+}
 
 
 function playRound(){
@@ -53,58 +78,59 @@ if (computerChoice == "rock"){
         console.log("Tie!");
     }
     else if (humanChoice == "paper"){
-        console.log("You Win!");
-        humanScore++;
+        win()
     }
-    else {console.log("You Lose!");
-        computerScore++;
+    else {lose()
+
+    }
 }
-}
+
 else if (computerChoice == "paper"){
     if (humanChoice == "rock"){
-        console.log("You Lose!");
-        computerScore++;
+        lose();
     }
     else if (humanChoice == "paper"){
         console.log("Tie!")}
     else {
-        console.log("You Win!");
-        humanScore++;
-}
+        win();
+    }
 }
 else if (computerChoice == "scissors"){
     if (humanChoice == "rock"){
-        console.log("You Win!");
-        humanScore++;
+        win();
     }
     else if (humanChoice == "paper"){
-        console.log("You Lose!");
-        computerScore++;
+        lose();
 }
     else {console.log("Tie!")
     }
 }
 round();
 playGame();
-roundNum++;
 }
 
 
 function round(){
+    roundNum++;
+
+    roundUi.innerText = roundNum;
+
     console.log(humanChoice + " Vs " + computerChoice + "...." + " The Score is now " + humanScore + " to " + computerScore);
 }
 function playGame(){
 if (roundNum === 5){
        
-     
         if (humanScore > computerScore) {
             console.log("Good Job! You have Defeated AI!")
+            winner.innertext = "Good Job! You have Defeated AI!";
         }
         else if (humanScore < computerScore) {
             console.log("You've been deafeated. Prepare for the AI takeover.")
+            winner.innerText = "You've been deafeated. Prepare for the AI takeover.";
         }
         else {
             console.log("A tie after 5 rounds? One more. This is your chance to Redeem yourself.");
+            winner.innerText = "A tie after 5 rounds? One more. This is your chance to Redeem yourself.";
             playRound();
         }
     return;
